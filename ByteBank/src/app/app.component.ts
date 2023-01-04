64cf05d0c0350efa3c88577058ea17bb9ad80de3
Yan Dias <yandiaspg@gmail.com>
2023-01-04 01:03:57 -0300

    component nova-transferencia

--- ByteBank/src/app/app.component.ts
+++ ByteBank/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ByteBank';
+  transferencia: any = {};
+
+  transferir($event) {
+    this.transferencia = $event
+  }
+
 }
--- ByteBank/src/app/app.module.ts
+++ ByteBank/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { NovaTransferenciaComponent } from './components/nova-transferencia/nova-transferencia.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
